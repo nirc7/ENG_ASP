0d507090d36d6578293c77c2bebbbdf0f471adb0
nirc7 <i2cs2013@gmail.com>
2019-05-14 21:28:58 +0300

    ver3

--- 12 3 Tier Layers/WebSite1/DALProj/DBServices.cs
+++ 12 3 Tier Layers/WebSite1/DALProj/DBServices.cs
@@ -3,21 +3,42 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DALProj
 {
-    public  class DBServices
+    public class DBServices
     {
-        static string conStr = @"Data Source=.;Initial Catalog=DBUsers;Integrated Security=True";
+        static string conStr = null;
         //static string conStr = null;
-        static bool local = false;
+        static bool local = true;
         static SqlConnection con = null;
         static SqlCommand comm = null;
 
-       static DBServices()
+        static DBServices()
         {
+            Configuration config = null;
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+
+            string exeConfigPath = path;
+            try
+            {
+                config = ConfigurationManager.OpenExeConfiguration(exeConfigPath);
+            }
+            catch (Exception ex)
+            {
+                //handle errror here.. means DLL has no sattelite configuration file.
+            }
+
+            if (config != null)
+            {
+                conStr = GetAppSetting(config, local? "Local" : "LiveDNS");
+            }
+
             //if (local)
             //{
             //    conStr = ConfigurationManager.ConnectionStrings["Local"].ConnectionString;
@@ -41,6 +62,18 @@ namespace DALProj
             con = new SqlConnection(conStr);
             comm = new SqlCommand();
             comm.Connection = con;
+        }
+
+        static  string GetAppSetting(Configuration config, string key)
+        {
+            KeyValueConfigurationElement element = config.AppSettings.Settings[key];
+            if (element != null)
+            {
+                string value = element.Value;
+                if (!string.IsNullOrEmpty(value))
+                    return value;
+            }
+            return string.Empty;
         }
 
         public static Person Login(string name, string pass)
